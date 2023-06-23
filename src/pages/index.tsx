5826d7f275f7632f18c1e5aaa64f2c1552160d7f
Allan Martinez <martinezallan123@gmail.com>
2023-06-22 21:01:37 -0700

    added stamp asset, setup coming soon page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,18 @@
+import { Stamp } from "@/components/Stamp";
 import Image from "next/image";
+import { Playfair_Display } from "next/font/google";
+
+const playfair = Playfair_Display({ subsets: ["latin"] });
 
 export default function Home() {
-  return <></>;
+  return (
+    <div className="h-screen w-screen flex justify-center">
+      <main className="flex flex-col items-center mt-40 gap-4">
+        <Stamp />
+        <h1 className="text-3xl">
+          <span className={playfair.className}>Coming Soon...</span>
+        </h1>
+      </main>
+    </div>
+  );
 }
