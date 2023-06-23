5332b9dd5110d3da1467b34986c89c8e4fd81127
Allan <martinezallan123@gmail.com>
2023-06-23 09:00:48 -0700

    add favicon files

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,11 +26,34 @@ export default function Home() {
           property="og:description"
           content="Discover our local barbershop in Calistoga, your go-to destination for exceptional haircuts and grooming services in the beautiful Napa Valley."
         />
+
+        {/* favicon */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
       </Head>
       <div className="h-screen w-screen flex justify-center items-center bg-black min-w-[320px] min-h-[500px]">
         <main className="flex flex-col justify-center items-center gap-4 text-white">
           <Image src={stamp} height={600} width={600} alt="fine lines logo" />
-          <h1 className="text-3xl">
+          <h1 className="text-3xl mt-4">
             <span className={playfair.className}>Coming Soon...</span>
           </h1>
 
