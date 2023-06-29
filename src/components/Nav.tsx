4fc44e932998fe7bc92cc1d9531c2db3233a9fce
Allan <martinezallan123@gmail.com>
2023-06-29 10:18:56 -0700

    created <Nav>

---
+++ src/components/Nav.tsx
@@ -0,0 +1,31 @@
+import Image from "next/image";
+import pole from "../assets/flpole.svg";
+import { BsInstagram } from "react-icons/bs";
+import { BsYelp } from "react-icons/bs";
+import { AiOutlinePhone } from "react-icons/ai";
+import { RxHamburgerMenu } from "react-icons/rx";
+
+export const Nav = () => {
+  return (
+    <nav className="flex justify-between items-center w-full p-2 fixed z-20 bg-black">
+      <ul className="SOCIALS justify-between flex items-center gap-5">
+        <li>
+          <a href="https://www.instagram.com/elchinoblends/" target="_blank">
+            <BsInstagram size={35} className="fill-white" />
+          </a>
+        </li>
+        <li>
+          <a>
+            <BsYelp size={35} className="fill-red-500" />
+          </a>
+        </li>
+        <li>
+          <a href="tel:707-752-7018">
+            <AiOutlinePhone size={35} className="fill-white"></AiOutlinePhone>
+          </a>
+        </li>
+      </ul>
+      <RxHamburgerMenu size={40} color="white" />
+    </nav>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,14 @@
 import { ComingSoon } from "@/components/ComingSoon";
 import { Layout } from "@/components/Layout";
 import Head from "next/head";
-
-//Nav
 import Image from "next/image";
-import pole from "../assets/flpole.svg";
-import { BsInstagram } from "react-icons/bs";
-import { BsYelp } from "react-icons/bs";
-import { AiOutlinePhone } from "react-icons/ai";
-import { RxHamburgerMenu } from "react-icons/rx";
 
 //Landing
 import stamp from "../assets/flstamp.svg";
 
 //Footer
 import { FiExternalLink } from "react-icons/fi";
+import { Nav } from "@/components/Nav";
 
 export default function Home() {
   return (
@@ -62,22 +56,20 @@ export default function Home() {
         <meta name="theme-color" content="#ffffff" />
       </Head>
       <Layout>
-        <nav className="flex justify-between items-center w-full p-2 fixed z-20 bg-black">
-          <div className="SOCIALS justify-between flex items-center gap-8">
-            <BsInstagram size={40}></BsInstagram>
-            <BsYelp size={40}></BsYelp>
-            <a href="tel:707-752-7018">
-              <AiOutlinePhone size={40}></AiOutlinePhone>
-            </a>
-          </div>
-          <RxHamburgerMenu size={40}></RxHamburgerMenu>
-        </nav>
+        <Nav />
         <main className="flex flex-col gap-4">
           <div className="flex flex-col relative w-screen h-screen">
             <div className="LANDING h-full w-full absolute bg-haircut bg-center bg-cover top-0 left-0"></div>
             <div className="SHADE bg-black bg-opacity-50 absolute h-full w-full top-0 left-0"></div>
             <header className="CONTENT z-10 mt-32 flex flex-col items-center">
-              <Image src={stamp} height={350} width={350} alt=""></Image>
+              <h1>
+                <Image
+                  src={stamp}
+                  height={350}
+                  width={350}
+                  alt="Fine Lines Barbershop"
+                ></Image>
+              </h1>
               <button className="py-3 px-9 bg-hl rounded-sm text-black mt-10">
                 Book Now
               </button>
