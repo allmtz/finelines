b1ebf9e0df097f13699359dbcc995931bd345732
Allan <martinezallan123@gmail.com>
2023-06-29 14:48:26 -0700

    add a temp border for dev purposes

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -5,7 +5,7 @@ export const Section: React.FunctionComponent<{
   title?: string;
 }> = ({ children, title }) => {
   return (
-    <section className="min-h-screen flex flex-col items-center p-2 gap-4">
+    <section className="border border-green-700 min-h-screen flex flex-col items-center p-2 gap-4">
       <h2 className="text-2xl self-start font-medium">{title}</h2>
       {children}
     </section>
