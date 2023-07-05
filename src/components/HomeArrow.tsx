9c60b03d2f46a0ef25cda59260e648a082dc5abd
Allan <martinezallan123@gmail.com>
2023-07-05 14:02:00 -0700

    adjust home arrow styling

--- src/components/HomeArrow.tsx
+++ src/components/HomeArrow.tsx
@@ -1,13 +1,13 @@
-import { BsFillArrowUpCircleFill } from "react-icons/bs";
+import { FaCircleArrowUp } from "react-icons/fa6";
 
 export const HomeArrow = () => {
   return (
     <div
       id="home-arrow"
-      className="hidden fixed bottom-4 right-4 z-10 bg-white rounded-full"
+      className="hidden fixed bottom-4 right-4 z-10 bg-white border border-white rounded-full"
     >
       <a href="#home">
-        <BsFillArrowUpCircleFill size={35} fill="black" />
+        <FaCircleArrowUp size={35} fill="black" />
       </a>
     </div>
   );
