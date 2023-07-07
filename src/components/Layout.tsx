4da08668d156b538f92c75815381e7ffb3e7a07b
Allan Martinez <martinezallan123@gmail.com>
2023-07-06 19:53:12 -0700

    adjust max width

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,7 @@
 import React, { ReactNode } from "react";
 
 export const Layout = ({ children }: { children: ReactNode }) => {
-  return <div className="w-screen max-w-7xl overflow-hidden">{children}</div>;
+  return (
+    <div className="w-screen max-w-[1440px] overflow-hidden">{children}</div>
+  );
 };
