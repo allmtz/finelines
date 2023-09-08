d33e9f4cc6cc2c724201ee7230e630f16fb8971a
Allan <martinezallan123@gmail.com>
2023-09-08 08:35:56 -0700

    use <Image> instead of bg image for hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,16 @@ import stamp from "../assets/flstamp.svg";
 export const Hero = () => {
   return (
     <div className="flex flex-col max-w-[1440px] relative w-screen h-screen overflow-hidden">
-      <div className="HERO h-full w-full absolute bg-haircut bg-center bg-cover top-0 left-0"></div>
+      <div className="HERO h-full w-full absolute">
+        <Image
+          src={"/images/haircut.jpg"}
+          alt="Fine Lines Barbershop"
+          className="object-cover"
+          placeholder="empty"
+          fill
+          priority={true}
+        />
+      </div>
       <div
         className="SHADE bg-gradient-to-b from-black via-transparent bg-black bg-opacity-50
         absolute h-full w-full top-0 left-0"
